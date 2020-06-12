43136989b37d9e5a85917c8a24887b94e4e29362
Olga Gidion <olga.gidion@gmail.com>
2020-06-12 21:12:43 +0200

    Changed structure in popup block

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 let editButton = document.querySelector('.profile__edit-button');
 
 editButton.addEventListener('click', function() {
-  let popupContainer = document.querySelector('.popup-container');
-  popupContainer.style.display = 'flex';
+  let popup = document.querySelector('.popup');
+  popup.style.display = 'flex';
 
-  let popupCloseButton = popupContainer.querySelector('.popup__close-button');
+  let popupCloseButton = popup.querySelector('.popup__close-button');
   popupCloseButton.addEventListener('click', function() {
-    popupContainer.style.display = 'none';
+    popup.style.display = 'none';
   })
 });
