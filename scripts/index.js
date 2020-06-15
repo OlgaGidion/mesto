5b0a11ced77f99ee7c050c486bb78acae9c72ba7
Olga Gidion <olga.gidion@gmail.com>
2020-06-15 16:02:50 +0200

    Fixed buttons

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@
 
 let profileTitle = document.querySelector('.profile__title');
 let profileText = document.querySelector('.profile__text');
-let editButton = document.querySelector('.profile__edit-button');
+let editButton = document.querySelector('.button_type_edit');
 let popup = document.querySelector('.popup');
-let popupCloseButton = popup.querySelector('.popup__close-button');
+let popupCloseButton = popup.querySelector('.button_type_close');
 let popupNameInput = popup.querySelector('.popup__input-name');
 let popupDescriptionInput = popup.querySelector('.popup__input-description');
 let popupForm = popup.querySelector('.popup__form');
